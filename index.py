77b89b6a601fd17c6108b538d541d5500fc1474b
Harish Chandrasekaran <Harish@Harishs-MacBook-Pro.local>
2019-02-27 03:28:27 -0500

    using different data set

--- index.py
+++ index.py
@@ -8,6 +8,6 @@ sns.set_palette("Dark2")
 df = pd.read_csv("data_fram.csv")
 print(df)
 
-ax = sns.scatterplot(x = "GS", y = "GP", data = df)
-ax.set_title("Number of Games vs. Goals Scored")
+ax = sns.scatterplot(x = "GP", y = "GS", data = df)
+ax.set_title("Number of Goals Scored vs. Games Played")
 plt.show()
