f7a039e3fd46a36f2d92eb269bb6ff7156114bda
Harish Chandrasekaran <Harish@harishs-mbp.lan>
2019-03-11 14:41:43 -0700

    changed data and some visualizations

--- .ipynb_checkpoints/index-checkpoint.py
+++ .ipynb_checkpoints/index-checkpoint.py
@@ -1,41 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 import webscraping
 
 sns.set(style="darkgrid")
 sns.set_palette("Dark2")
 
-'''
-webscraping.get_goal_stats()
-webscraping.get_assist_stats()
-webscraping.get_shot_stats()
-webscraping.get_shotOnGoal_stats()
-webscraping.get_post_stats()
-webscraping.get_passes_stats()
-webscraping.get_offsides_stats()
-webscraping.get_crosses_stats()
-webscraping.get_yellow_stats()
-webscraping.get_red_stats()
-webscraping.get_touch_stats()
-webscraping.get_block_stats()
-webscraping.get_int_stats()
-webscraping.get_tackle_stats()
-webscraping.get_clear_stats()
-webscraping.get_error_stats()
-webscraping.get_pen_stats()
-webscraping.get_foul_stats()
-webscraping.get_cleanSheet_stats()
-webscraping.get_goalsAgainst_stats()
-webscraping.get_saves_stats()
-webscraping.get_punches_stats()
-webscraping.get_highClaims_stats()
-webscraping.get_ownGoals_stats()
-'''
 
-attack_df = pd.read_csv("goal_data.csv")
-attack_df = pd.read_csv("assist_data.csv")
-attack_df.to_csv('attack_data.csv')
+#webscraping.get_premStandard_stats()
+
+
+
+
+
 
 '''
 df = pd.read_csv("data_fram.csv")
--- webscraping.py
+++ webscraping.py
@@ -3,290 +3,46 @@ import pandas as pd
 from selenium import webdriver
 import time
 
-def get_goal_stats():
+def get_premStandard_stats():
     browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/goals')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
+    browser.get('https://www.foxsports.com/soccer/stats?competition=1&season=20180&category=STANDARD')
+    time.sleep(3)
+    table = browser.find_element_by_class_name('wisbb_standardTable').get_attribute('outerHTML')
     data = pd.read_html(table)
     data_df = data[0]
     print(data_df)
-    data_df.to_csv('goal_data.csv')
-    browser.close()
-
-def get_assist_stats():
-    browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/goal_assist')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
-    data = pd.read_html(table)
-    data_df = data[0]
-    print(data_df)
-    data_df.to_csv('assist_data.csv')
+    data_df.to_csv('premStandard_data.csv')
     browser.close()
     
-def get_shot_stats():
+def get_premForward_stats():
     browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/total_scoring_att')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
+    browser.get('https://www.foxsports.com/soccer/stats?competition=1&season=20180&category=STANDARD&pos=Forward&team=0&isOpp=0&sort=3&sortOrder=0')
+    time.sleep(3)
+    table = browser.find_element_by_class_name('wisbb_standardTable').get_attribute('outerHTML')
     data = pd.read_html(table)
     data_df = data[0]
     print(data_df)
-    data_df.to_csv('shots_data.csv')
+    data_df.to_csv('premForward_data.csv')
     browser.close()
     
-def get_shotOnGoal_stats():
+def get_premMidfield_stats():
     browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/ontarget_scoring_att')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
+    browser.get('https://www.foxsports.com/soccer/stats?competition=1&season=20180&category=STANDARD&pos=Midfielder&team=0&isOpp=0&sort=3&sortOrder=0')
+    time.sleep(3)
+    table = browser.find_element_by_class_name('wisbb_standardTable').get_attribute('outerHTML')
     data = pd.read_html(table)
     data_df = data[0]
     print(data_df)
-    data_df.to_csv('shotsOnGoal_data.csv')
+    data_df.to_csv('premMidfield_data.csv')
     browser.close()
     
-def get_post_stats():
+def get_premDefense_stats():
     browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/hit_woodwork')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
+    browser.get('https://www.foxsports.com/soccer/stats?competition=1&season=20180&category=STANDARD&pos=Defender&team=0&isOpp=0&sort=3&sortOrder=0')
+    time.sleep(3)
+    table = browser.find_element_by_class_name('wisbb_standardTable').get_attribute('outerHTML')
     data = pd.read_html(table)
     data_df = data[0]
     print(data_df)
-    data_df.to_csv('post_data.csv')
-    browser.close()
-
-def get_passes_stats():
-    browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/total_pass')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
-    data = pd.read_html(table)
-    data_df = data[0]
-    print(data_df)
-    data_df.to_csv('pass_data.csv')
-    browser.close()
-    
-def get_offsides_stats():
-    browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/total_offside')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
-    data = pd.read_html(table)
-    data_df = data[0]
-    print(data_df)
-    data_df.to_csv('offsides_data.csv')
-    browser.close()
-    
-def get_crosses_stats():
-    browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/total_cross')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
-    data = pd.read_html(table)
-    data_df = data[0]
-    print(data_df)
-    data_df.to_csv('crosses_data.csv')
-    browser.close()
-    
-def get_yellow_stats():
-    browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/yellow_card')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
-    data = pd.read_html(table)
-    data_df = data[0]
-    print(data_df)
-    data_df.to_csv('yellow_data.csv')
-    browser.close()
-    
-def get_red_stats():
-    browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/red_card')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
-    data = pd.read_html(table)
-    data_df = data[0]
-    print(data_df)
-    data_df.to_csv('red_data.csv')
-    browser.close()
-    
-def get_touch_stats():
-    browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/touches')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
-    data = pd.read_html(table)
-    data_df = data[0]
-    print(data_df)
-    data_df.to_csv('touch_data.csv')
-    browser.close()
-    
-def get_block_stats():
-    browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/outfielder_block')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
-    data = pd.read_html(table)
-    data_df = data[0]
-    print(data_df)
-    data_df.to_csv('block_data.csv')
-    browser.close()
-    
-def get_int_stats():
-    browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/interception')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
-    data = pd.read_html(table)
-    data_df = data[0]
-    print(data_df)
-    data_df.to_csv('int_data.csv')
-    browser.close()
-
-def get_tackle_stats():
-    browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/total_tackle')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
-    data = pd.read_html(table)
-    data_df = data[0]
-    print(data_df)
-    data_df.to_csv('tackle_data.csv')
-    browser.close()
-    
-def get_clear_stats():
-    browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/total_clearance')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
-    data = pd.read_html(table)
-    data_df = data[0]
-    print(data_df)
-    data_df.to_csv('clear_data.csv')
-    browser.close()
-    
-def get_error_stats():
-    browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/error_lead_to_goal')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
-    data = pd.read_html(table)
-    data_df = data[0]
-    print(data_df)
-    data_df.to_csv('error_data.csv')
-    browser.close()
-
-def get_pen_stats():
-    browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/penalty_conceded')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
-    data = pd.read_html(table)
-    data_df = data[0]
-    print(data_df)
-    data_df.to_csv('pen_data.csv')
-    browser.close()
-    
-def get_foul_stats():
-    browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/fouls')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
-    data = pd.read_html(table)
-    data_df = data[0]
-    print(data_df)
-    data_df.to_csv('foul_data.csv')
-    browser.close()
-
-def get_cleanSheet_stats():
-    browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/clean_sheet?po=GOALKEEPER')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
-    data = pd.read_html(table)
-    data_df = data[0]
-    print(data_df)
-    data_df.to_csv('cleanSheet_data.csv')
-    browser.close()
-    
-def get_goalsAgainst_stats():
-    browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/goals_conceded?po=GOALKEEPER')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
-    data = pd.read_html(table)
-    data_df = data[0]
-    print(data_df)
-    data_df.to_csv('goalsAgainst_data.csv')
-    browser.close()
-    
-def get_saves_stats():
-    browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/saves?po=GOALKEEPER')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
-    data = pd.read_html(table)
-    data_df = data[0]
-    print(data_df)
-    data_df.to_csv('saves_data.csv')
-    browser.close()
-    
-def get_punches_stats():
-    browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/punches?po=GOALKEEPER')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
-    data = pd.read_html(table)
-    data_df = data[0]
-    print(data_df)
-    data_df.to_csv('punches_data.csv')
-    browser.close()
-
-def get_highClaims_stats():
-    browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/total_high_claim?po=GOALKEEPER')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
-    data = pd.read_html(table)
-    data_df = data[0]
-    print(data_df)
-    data_df.to_csv('highClaims_data.csv')
-    browser.close()
-    
-def get_ownGoals_stats():
-    browser = webdriver.Firefox()
-    browser.get('https://www.premierleague.com/stats/top/players/own_goals')
-    time.sleep(10)
-
-    table = browser.find_element_by_class_name('table.statsTable').get_attribute('outerHTML')
-    data = pd.read_html(table)
-    data_df = data[0]
-    print(data_df)
-    data_df.to_csv('ownGoals_data.csv')
+    data_df.to_csv('premDefense_data.csv')
     browser.close()
