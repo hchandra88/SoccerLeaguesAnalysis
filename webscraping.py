f51ca23c684136e3954984bade2e93a619044f69
Harish Chandrasekaran <Harish@harishs-mbp.lan>
2019-03-12 23:10:12 -0700

    top 5 leagues

--- webscraping.py
+++ webscraping.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 from selenium import webdriver
 import time
@@ -13,52 +12,43 @@ def get_premTopGoals():
     data_df = data[0]
     data_df.to_csv('premTopGoals.csv')
     browser.close()
-
-
-
-
-
-
-def get_premStandard_stats():
+    
+def get_laligaTopGoals():
     browser = webdriver.Firefox()
-    browser.get('https://www.foxsports.com/soccer/stats?competition=1&season=20180&category=STANDARD')
+    browser.get('https://www.foxsports.com/soccer/stats?competition=2&season=20180&category=STANDARD&pos=0&team=0&sort=3&sortOrder=0')
     time.sleep(3)
     table = browser.find_element_by_class_name('wisbb_standardTable').get_attribute('outerHTML')
     data = pd.read_html(table)
     data_df = data[0]
-    print(data_df)
-    data_df.to_csv('premStandard_data.csv')
+    data_df.to_csv('laligaTopGoals.csv')
     browser.close()
     
-def get_premForward_stats():
+def get_bundTopGoals():
     browser = webdriver.Firefox()
-    browser.get('https://www.foxsports.com/soccer/stats?competition=1&season=20180&category=STANDARD&pos=Forward&team=0&isOpp=0&sort=3&sortOrder=0')
+    browser.get('https://www.foxsports.com/soccer/stats?competition=4&season=20180&category=STANDARD&pos=0&team=0&sort=3&sortOrder=0')
     time.sleep(3)
     table = browser.find_element_by_class_name('wisbb_standardTable').get_attribute('outerHTML')
     data = pd.read_html(table)
     data_df = data[0]
-    print(data_df)
-    data_df.to_csv('premForward_data.csv')
+    data_df.to_csv('bundTopGoals.csv')
     browser.close()
     
-def get_premMidfield_stats():
+def get_ligue1TopGoals():
     browser = webdriver.Firefox()
-    browser.get('https://www.foxsports.com/soccer/stats?competition=1&season=20180&category=STANDARD&pos=Midfielder&team=0&isOpp=0&sort=3&sortOrder=0')
+    browser.get('https://www.foxsports.com/soccer/stats?competition=43&season=20180&category=STANDARD&pos=0&team=0&sort=3&sortOrder=0')
     time.sleep(3)
     table = browser.find_element_by_class_name('wisbb_standardTable').get_attribute('outerHTML')
     data = pd.read_html(table)
     data_df = data[0]
-    print(data_df)
-    data_df.to_csv('premMidfield_data.csv')
+    data_df.to_csv('ligue1TopGoals.csv')
     browser.close()
     
-def get_premDefense_stats():
+def get_serieATopGoals():
     browser = webdriver.Firefox()
-    browser.get('https://www.foxsports.com/soccer/stats?competition=1&season=20180&category=STANDARD&pos=Defender&team=0&isOpp=0&sort=3&sortOrder=0')
+    browser.get('https://www.foxsports.com/soccer/stats?competition=3&season=20180&category=STANDARD&pos=0&team=0&sort=3&sortOrder=0')
     time.sleep(3)
     table = browser.find_element_by_class_name('wisbb_standardTable').get_attribute('outerHTML')
     data = pd.read_html(table)
     data_df = data[0]
-    print(data_df)
-    data_df.to_csv('premDefense_data.csv')
+    data_df.to_csv('serieATopGoals.csv')
     browser.close()
